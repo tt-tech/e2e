e80205bd72ae9efa247ee6edfea4b6f903161b15
Adil Ben Moussa <adil.benmoussa@gmail.com>
2021-02-01 15:24:03 +0100

    feat: add helper scripts

--- e2e/fixtures/hello.test.js
+++ e2e/fixtures/hello.test.js
@@ -1,4 +1,6 @@
-fixture('Welcome!');
+import {URLS} from '../utils/route.util';
+
+fixture('Welcome login!').page(URLS.LOGIN);
 
 test('should welcome the new project', async t => {
   await t.expect(1).eql(1);
