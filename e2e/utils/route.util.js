7ea2516fc410af058f1fa68232d5ab1f4ba1ebd4
Mohamed <benmoussa96med@gmail.com>
2021-02-01 18:24:00 +0100

    Update route.util.js

--- e2e/utils/route.util.js
+++ e2e/utils/route.util.js
@@ -1,9 +1,9 @@
-import {baseUrl} from './constant.util';
+import { baseUrl } from './constant.util';
 
 export function composePath(route) {
-  return `${baseUrl}/#/${route}`;
+    return `${baseUrl}/#/${route}`;
 }
 
 export const URLS = {
-  LOGIN: composePath('login')
+    LOGIN: composePath('login')
 };
