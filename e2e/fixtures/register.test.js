aa599d0869877ea7cf8c416b9255c4f05f080994
Mohamed <benmoussa96med@gmail.com>
2021-02-02 13:12:58 +0100

    Update register.test.js

--- e2e/fixtures/register.test.js
+++ e2e/fixtures/register.test.js
@@ -1,7 +1,5 @@
-import { URLS } from '../utils/route.util';
+import {URLS} from '../utils/route.util';
 
 fixture('Welcome For Register!').page(URLS.REGISTER);
 
-test('test for new user', async t => {
-
-});
+test('test for new user', async t => {});
