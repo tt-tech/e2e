80fbd733502e20941eaf5ce471464b54aba0ad35
Mohamed <benmoussa96med@gmail.com>
2021-02-02 13:13:00 +0100

    Update hello.test.js

--- e2e/fixtures/hello.test.js
+++ e2e/fixtures/hello.test.js
@@ -1,7 +1,7 @@
-import { URLS } from '../utils/route.util';
+import {URLS} from '../utils/route.util';
 
 fixture('Welcome login!').page(URLS.LOGIN);
 
 test('should welcome the new project', async t => {
-    await t.expect(1).eql(1);
+  await t.expect(1).eql(1);
 });
