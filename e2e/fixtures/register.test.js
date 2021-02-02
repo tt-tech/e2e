ab4b17b4458d003ee19352116cb1b77a1caeef4b
Mohamed <benmoussa96med@gmail.com>
2021-02-02 16:51:11 +0100

    Update register.test.js

--- e2e/fixtures/register.test.js
+++ e2e/fixtures/register.test.js
@@ -1,28 +1,22 @@
-import {URLS} from '../utils/route.util';
-import {Selector} from 'testcafe';
-//Declare  chance js
+import { URLS } from '../utils/route.util';
+import { RegisterElements } from '../page-objects/register.po';
+
 const Chance = require('chance');
 const chanceObj = new Chance();
-//declare Selector
-const name = Selector('#name');
-const email = Selector('#email');
-const password = Selector('#password');
-const Btnregister = Selector('button').withText('Register');
 
-//work with chance js
 const User = chanceObj.name();
 const Username = User.toLowerCase();
 const Email = Username.replace(' ', '-') + '@gmail.com';
-const passwords = 'Testing';
+const passwords = "Testing";
+
 fixture('Welcome For Register!').page(URLS.REGISTER);
 
 test('test for new user', async t => {
-  //Act
-  await t
-    .debug()
-    .typeText(name, Username)
-    .typeText(email, Email)
-    .typeText(password, passwords)
-    //.expect(passwords.length > 6)
-    .click(Btnregister);
+    //Act
+    await t
+        .typeText(RegisterElements.name, Username)
+        .typeText(RegisterElements.email, Email)
+        .typeText(RegisterElements.password, passwords)
+        //.expect(passwords.length > 6)
+        .click(RegisterElements.Btnregister)
 });
