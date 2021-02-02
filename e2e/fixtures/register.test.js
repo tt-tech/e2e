08289e2ddec8c264cc2f3fd3ffe225eb4d7d1aea
Mohamed <benmoussa96med@gmail.com>
2021-02-02 15:55:52 +0100

    Update register.test.js

--- e2e/fixtures/register.test.js
+++ e2e/fixtures/register.test.js
@@ -1,23 +1,27 @@
 import { URLS } from '../utils/route.util';
 import { Selector } from 'testcafe';
-//Declare  chance js 
+import { url } from 'inspector';
+//Declare  chance js
 const Chance = require('chance');
 const chanceObj = new Chance();
 //declare Selector
 const name = Selector('#name');
 const email = Selector('#email');
 const password = Selector('#password');
+const Btnregister = Selector('button').withText('Register');
 
 //work with chance js
-const User = chanceObj.name().toLowerCase;
-const Email = User.replace(' ', '-') + '@gmail.com';
-const password = "Testing";
-
+const User = chanceObj.name();
+const Username = User.toLowerCase();
+const Email = Username.replace(' ', '-') + '@gmail.com';
+const passwords = "Testing";
 fixture('Welcome For Register!').page(URLS.REGISTER);
-test('test for new user', async t => {
 
+test('test for new user', async t => {
     await t
         .debug()
-        .typeText(name, 'Mohamed')
-        .typeText(email, 'Mohamed-12@gmail.com')
+        .typeText(name, Username)
+        .typeText(email, Email)
+        .typeText(password, passwords)
+        .click(Btnregister)
 });
