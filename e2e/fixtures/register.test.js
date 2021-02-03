d28f6303dafc62dfc1f3120ee7313d0ee00f2b7f
Mohamed <benmoussa96med@gmail.com>
2021-02-03 09:40:00 +0100

    Update register.test.js

--- e2e/fixtures/register.test.js
+++ e2e/fixtures/register.test.js
@@ -4,9 +4,9 @@ import { RegisterElements } from '../page-objects/register.po';
 const Chance = require('chance');
 const chanceObj = new Chance();
 
-const User = chanceObj.name();
-const Username = User.toLowerCase();
-const Email = Username.replace(' ', '-') + '@gmail.com';
+const user = chanceObj.name();
+const username = user.toLowerCase();
+const email = username.replace(' ', '-') + '@gmail.com';
 const passwords = "Testing";
 
 fixture('Welcome For Register!').page(URLS.REGISTER);
@@ -14,9 +14,8 @@ fixture('Welcome For Register!').page(URLS.REGISTER);
 test('test for new user', async t => {
     //Act
     await t
-        .typeText(RegisterElements.name, Username)
-        .typeText(RegisterElements.email, Email)
+        .typeText(RegisterElements.name, username)
+        .typeText(RegisterElements.email, email)
         .typeText(RegisterElements.password, passwords)
-        //.expect(passwords.length > 6)
         .click(RegisterElements.Btnregister)
 });
