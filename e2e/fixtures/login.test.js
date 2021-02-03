7b6a0e1585fa86fa1f2837b585daa77974446821
Mohamed <benmoussa96med@gmail.com>
2021-02-03 16:58:36 +0100

    Update login.test.js

--- e2e/fixtures/login.test.js
+++ e2e/fixtures/login.test.js
@@ -1,17 +1,19 @@
-import {URLS} from '../utils/route.util';
-import {loginElements} from '../page-objects/login.po';
+import { URLS } from '../utils/route.util';
+import { loginElements } from '../page-objects/login.po';
+
 fixture('Welcome For login').page(URLS.LOGIN);
 
 test('test for login ', async t => {
-  //Arrange
-  const email = 'chester-mills@gmail.com';
-  const password = 'Testing';
-  const med = 'Hello,' + email;
-  //Act
-  await t
-    .typeText(loginElements.email, email)
-    .typeText(loginElements.password, password)
-    .click(loginElements.btnLogin);
-  //Assert
-  t.expect(loginElements.helloUser.innerText).contains(email);
+    //Arrange
+    const email = 'chester-mills@gmail.com';
+    const password = 'Testing';
+
+    //Act
+    await t
+        .typeText(loginElements.email, email)
+        .typeText(loginElements.password, password)
+        .click(loginElements.btnLogin)
+
+    //Assert
+    await t.expect(loginElements.helloUser.innerText).contains(email);
 });
