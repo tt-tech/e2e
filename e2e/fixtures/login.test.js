92ec15ae90251fe4653f2de7a65f4f2432cfbdaa
Mohamed <benmoussa96med@gmail.com>
2021-02-03 12:34:51 +0100

    Update login.test.js

--- e2e/fixtures/login.test.js
+++ e2e/fixtures/login.test.js
@@ -1,7 +1,13 @@
-import {URLS} from '../utils/route.util';
-import {loginElements} from '../page-objects/login.po';
-fixture('Welcome login!').page(URLS.LOGIN);
-
+import { URLS } from '../utils/route.util';
+import { loginElements } from '../page-objects/login.po';
+fixture('Welcome For login').page(URLS.LOGIN);
+//Arrange
+const email = 'chester-mills@gmail.com';
+const password = 'Testing';
 test('should welcome the new project', async t => {
-  await t.typeText(loginElements.email).typeText(loginElements.password);
+    await t
+        .debug()
+        .typeText(loginElements.email, email)
+        .typeText(loginElements.password, password)
+        .click(loginElements.btnLogin)
 });
