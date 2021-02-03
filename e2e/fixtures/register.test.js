350ed381ae4ad9ec8a18c845cde97b6bd4f0b4d5
Mohamed <benmoussa96med@gmail.com>
2021-02-03 18:07:38 +0100

    Update register.test.js

--- e2e/fixtures/register.test.js
+++ e2e/fixtures/register.test.js
@@ -1,21 +1,21 @@
-import { URLS } from '../utils/route.util';
-import { RegisterElements } from '../page-objects/register.po';
-import { generatePerson } from '../utils/person.util';
+import {URLS} from '../utils/route.util';
+import {RegisterElements} from '../page-objects/register.po';
+import {generatePerson} from '../utils/person.util';
 
 fixture('Welcome For Register page').page(URLS.REGISTER);
 
 test('test for new user', async t => {
-    //Arrange
-    const person = generatePerson();
+  //Arrange
+  const person = generatePerson();
 
-    //Act
-    await t
-        .debug()
-        .typeText(RegisterElements.name, person.username)
-        .typeText(RegisterElements.email, person.email)
-        .typeText(RegisterElements.password, person.password)
-        .click(RegisterElements.btnRegister);
+  //Act
+  await t
+    .debug()
+    .typeText(RegisterElements.name, person.username)
+    .typeText(RegisterElements.email, person.email)
+    .typeText(RegisterElements.password, person.password)
+    .click(RegisterElements.btnRegister);
 
-    //Assert
-    //await t.expect(RegisterElements.helloUser.innerText).contains(person.email);
+  //Assert
+  //await t.expect(RegisterElements.helloUser.innerText).contains(person.email);
 });
