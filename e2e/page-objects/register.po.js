aea50e74390a7bccf242ba5866f26fa41e1e6aaa
Mohamed <benmoussa96med@gmail.com>
2021-02-03 18:07:29 +0100

    Update register.po.js

--- e2e/page-objects/register.po.js
+++ e2e/page-objects/register.po.js
@@ -1,10 +1,9 @@
-import { Selector } from 'testcafe';
+import {Selector} from 'testcafe';
 
 export const RegisterElements = {
-    name: Selector('#name'),
-    email: Selector('#email'),
-    password: Selector('#password'),
-    btnRegister: Selector('button').withText('Register'),
-    helloUser: Selector('#hello-user')
-
+  name: Selector('#name'),
+  email: Selector('#email'),
+  password: Selector('#password'),
+  btnRegister: Selector('button').withText('Register'),
+  helloUser: Selector('#hello-user')
 };
