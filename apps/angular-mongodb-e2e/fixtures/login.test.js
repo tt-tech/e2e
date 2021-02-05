b469e2c65921dfdaf9445bad871f6c68fed583b0
Mohamed <benmoussa96med@gmail.com>
2021-02-05 22:11:12 +0100

    Update login.test.js

--- apps/angular-mongodb-e2e/fixtures/login.test.js
+++ apps/angular-mongodb-e2e/fixtures/login.test.js
@@ -16,7 +16,7 @@ test.only('test for login ', async t => {
 
     //Assert
     //await t.eval(() => location.reload(true));
-    await t.pressKey('CMD+R');
+    await t.pressKey('Command+R');
     await t.wait(4000)
         //     await t.expect(RegisterElements.username.exists).ok();
         //     await t.expect(RegisterElements.helloUser.innerText).contains(username);
