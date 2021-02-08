c975bee29738d1cbb03fd80041a7f9ed69fa2ca6
Mohamed <benmoussa96med@gmail.com>
2021-02-08 13:37:34 +0100

    Update register.test.js

--- apps/angular-mongodb-e2e/fixtures/register.test.js
+++ apps/angular-mongodb-e2e/fixtures/register.test.js
@@ -1,4 +1,5 @@
 import {URLS} from '../utils/route.util';
+import {password} from '../utils/constant.util';
 import {registerElements} from '../page-objects/register.po';
 import {generatePerson} from '../utils/person.util';
 import {loginElements} from '../page-objects/login.po';
@@ -10,16 +11,16 @@ test.only('test for new user ', async t => {
   const person = generatePerson();
   //Act
   await t
-    .debug()
     .typeText(registerElements.userName, person.username)
     .typeText(registerElements.email, person.email)
-    .typeText(registerElements.password, person.password)
+    .typeText(registerElements.password, password)
     .click(registerElements.btnRegister)
     .typeText(loginElements.username, person.username)
-    .typeText(loginElements.password, person.password)
+    .typeText(loginElements.password, password)
     .click(loginElements.btnLogin);
-  //Assert
   await t.eval(() => location.reload(true));
+  //Assert
   await t.expect(loginElements.helloUser.exists).ok();
   await t.expect(loginElements.helloUser.innerText).contains(person.username);
+  await t.expect(loginElements.logOut.innerText).contains(person.logOut);
 });
