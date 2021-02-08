ed91fa390ac213498e93e19f08960a5490da63d8
Mohamed <benmoussa96med@gmail.com>
2021-02-08 12:42:48 +0100

    Update register.test.js

--- apps/angular-mongodb-e2e/fixtures/register.test.js
+++ apps/angular-mongodb-e2e/fixtures/register.test.js
@@ -1,6 +1,8 @@
 import {URLS} from '../utils/route.util';
 import {registerElements} from '../page-objects/register.po';
 import {generatePerson} from '../utils/person.util';
+import {loginElements} from '../page-objects/login.po';
+
 fixture('Welcome For register page ').page(URLS.REGISTER);
 
 test.only('test for new user ', async t => {
@@ -12,7 +14,12 @@ test.only('test for new user ', async t => {
     .typeText(registerElements.userName, person.username)
     .typeText(registerElements.email, person.email)
     .typeText(registerElements.password, person.password)
-    .click(registerElements.btnRegister);
-    
+    .click(registerElements.btnRegister)
+    .typeText(loginElements.username, person.username)
+    .typeText(loginElements.password, person.password)
+    .click(loginElements.btnLogin);
   //Assert
+  await t.eval(() => location.reload(true));
+  await t.expect(loginElements.helloUser.exists).ok();
+  await t.expect(loginElements.helloUser.innerText).contains(person.username);
 });
