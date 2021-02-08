586f4a6154437c60318120ead45da0abb68c9c36
Mohamed <benmoussa96med@gmail.com>
2021-02-08 12:42:43 +0100

    Update login.test.js

--- apps/angular-mongodb-e2e/fixtures/login.test.js
+++ apps/angular-mongodb-e2e/fixtures/login.test.js
@@ -1,21 +1,19 @@
-import { URLS } from '../utils/route.util';
-import { loginElements } from '../page-objects/login.po';
+import {URLS} from '../utils/route.util';
+import {loginElements} from '../page-objects/login.po';
 
 fixture('Welcome For login').page(URLS.LOGIN);
 
 test.only('test for login ', async t => {
-    //Arrange
-    const username = 'steven.reyes';
-    const password = 'Testing';
-
-    //Act
-    await t
-        .typeText(loginElements.username, username)
-        .typeText(loginElements.password, password)
-        .click(loginElements.btnLogin);
-
-    //Assert
-    await t.eval(() => window.location.reload);
-    // await t.expect(RegisterElements.username.exists).ok();
-    // await t.expect(RegisterElements.helloUser.innerText).contains(username);
+  //Arrange
+  const username = 'tony.perry';
+  const password = 'Testing';
+  //Act
+  await t
+    .typeText(loginElements.username, username)
+    .typeText(loginElements.password, password)
+    .click(loginElements.btnLogin);
+  //Assert
+  await t.eval(() => location.reload(true));
+  await t.expect(loginElements.helloUser.exists).ok();
+  await t.expect(loginElements.helloUser.innerText).contains(username);
 });
