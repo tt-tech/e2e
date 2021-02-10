8d8a41a34a05cbcb47b00130874c00de35c7b0a0
Mohamed <benmoussa96med@gmail.com>
2021-02-10 11:13:47 +0100

    Update register.po.js

--- apps/angular-mongodb-e2e/page-objects/register.po.js
+++ apps/angular-mongodb-e2e/page-objects/register.po.js
@@ -4,7 +4,8 @@ export const registerElements = {
   userName: Selector('#username'),
   email: Selector('#email'),
   password: Selector('#password'),
-  btnRegister: Selector('button').withText('Sign Up')
+  btnRegister: Selector('button').withText('Sign Up'),
+  msgSuccess: Selector('#alert')
 };
 
 export const getUrl = ClientFunction(() => window.location.href);
