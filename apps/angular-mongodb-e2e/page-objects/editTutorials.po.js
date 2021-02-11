10b4e7183dc86a6b5976d4fd3fe43240302d0d89
Mohamed <benmoussa96med@gmail.com>
2021-02-11 17:06:46 +0100

    Update editTutorials.po.js

--- apps/angular-mongodb-e2e/page-objects/editTutorials.po.js
+++ apps/angular-mongodb-e2e/page-objects/editTutorials.po.js
@@ -1,37 +1,46 @@
-import { Selector } from 'testcafe';
+import {Selector} from 'testcafe';
 
 export const tutorialsElements = {
-    btnAdd: Selector('a').withText('Add'),
-    title: Selector('#title'),
-    description: Selector('#description'),
-    btnSubmit: Selector('button').withText('Submit'),
-    msgSucces: Selector('h4').withText('Tutorial was submitted successfully!')
+  btnAdd: Selector('a').withText('Add'),
+  title: Selector('#title'),
+  description: Selector('#description'),
+  btnSubmit: Selector('button').withText('Submit'),
+  msgSucces: Selector('h4').withText('Tutorial was submitted successfully!')
 };
 
 export const updateToturial = {
-    Tutorials: Selector('a').withText('Tutorials'),
-    listTutorial: Selector('ul.list-group').child('li').nth(-1),
-    btnEdit: Selector('a').withText('Edit'),
-    title: Selector('#title'),
-    description: Selector('#description'),
-    btnUpdate: Selector('button').withText('Update'),
-    mgsSucces: Selector('p').withText('Tutorial was updated successfully.')
+  Tutorials: Selector('a').withText('Tutorials'),
+  listTutorial: Selector('ul.list-group').child('li').nth(-1),
+  btnEdit: Selector('a').withText('Edit'),
+  title: Selector('#title'),
+  description: Selector('#description'),
+  btnUpdate: Selector('button').withText('Update'),
+  mgsSucces: Selector('p').withText('Tutorial was updated successfully.')
 };
 
 export const publishedTutorials = {
-    Tutorials: Selector('a').withText('Tutorials'),
-    listTutorial: Selector('ul.list-group').child('li').nth(0),
-    btnEdit: Selector('a').withText('Edit'),
-    btnPublish: Selector('button').withText('Publish'),
-    status: Selector('div.form-group').withText('Published'),
-    msgAccess: Selector('p').withText('Tutorial was updated successfully.')
+  Tutorials: Selector('a').withText('Tutorials'),
+  listTutorial: Selector('ul.list-group').child('li').nth(0),
+  btnEdit: Selector('a').withText('Edit'),
+  btnPublish: Selector('button').withText('Publish'),
+  status: Selector('div.form-group').withText('Published'),
+  msgAccess: Selector('p').withText('Tutorial was updated successfully.')
 };
 
 export const UnPublishedTutorials = {
-    Tutorials: Selector('a').withText('Tutorials'),
-    listTutorial: Selector('ul.list-group').child('li').nth(0),
-    btnEdit: Selector('a').withText('Edit'),
-    btnPublish: Selector('button').withText('Publish'),
-    status: Selector('div.form-group').withText('Pending'),
-    msgAccess: Selector('p').withText('Tutorial was updated successfully.')
+  Tutorials: Selector('a').withText('Tutorials'),
+  listTutorial: Selector('ul.list-group').child('li').nth(0),
+  btnEdit: Selector('a').withText('Edit'),
+  btnPublish: Selector('button').withText('Publish'),
+  status: Selector('div.form-group').withText('Pending'),
+  msgAccess: Selector('p').withText('Tutorial was updated successfully.')
 };
+
+export const deleteTutorials = {
+  Tutorials: Selector('a').withText('Tutorials'),
+  listTutorial: Selector('ul.list-group').child('li').nth(0),
+  btnEdit: Selector('a').withText('Edit'),
+  btnDelete: Selector('button').withText('Delete')
+};
+
+// export const getUrl = ClientFunction(() => window.location.href);
